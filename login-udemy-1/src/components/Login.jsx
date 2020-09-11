56ff766e706bfec98c815799eed3443144d7f8d1
Pablo2e <hebenstreitpablo@hotmail.com>
2020-09-11 18:54:37 +0200

    Auth. Rutas protegidas. Autenticación con Firebase parte 2

--- login-udemy-1/src/components/Login.jsx
+++ login-udemy-1/src/components/Login.jsx
@@ -4,30 +4,49 @@ const Login = () => {
 
     const [email, setEmail] = React.useState('')
     const [password, setPaswsword] = React.useState('')
+    const [error, setError] = React.useState(null)
+    const [esRegistro, setEsRegistro] = React.useState(true)
+
 
     const procesarDatos = e => {
         e.preventDefault()
         if(!email.trim()){
-            console.log('Ingrese Email')
+            // console.log('Ingrese Email')
+            setError('Ingrese Email')
             return
         }
         if(!password.trim()){
-            console.log('Ingrese Password')
+            // console.log('Ingrese Password')
+            setError('Ingrese Password')
             return
         }
         if(password.length < 6){
-            console.log('Tu contraseña debe tener mas de 6 caracteres')
+            // console.log('Tu contraseña debe tener mas de 6 caracteres')
+            setError('Tu contraseña debe tener más de 6 caracteres')
+            return
         }
+        setError(null)
         console.log('Pasando todas las validaciones')
     }
 
     return (
         <div className="mt-5">
-           <h3 className="text-center">Registro de usuarios</h3>
+           <h3 className="text-center">
+               {
+                  esRegistro ? 'Registro de usuarios' : 'Login de acceso' 
+               }
+           </h3>
            <hr/>
            <div className="row justify-content-center">
                <div className="col-12 col-sm-8 col-md-6 col-xl-4">
                    <form onSubmit={procesarDatos}>
+                       {
+                            error && (
+                                <div className="alert alert-danger">
+                                    {error}
+                                </div>
+                            )   
+                       }
                        <input 
                         type="email"
                         className="form-control mb-2"
@@ -42,11 +61,20 @@ const Login = () => {
                         onChange={e => setPaswsword(e.target.value)}
                         value={password}
                     />
-                    <button className="btn btn-dark btn-lg btn-block">
-                        Registrarse
+                    <button className="btn btn-dark btn-lg btn-block"
+                            type="submit"
+                    >
+                        {
+                            esRegistro ? 'Registrarse' : 'Acceder'
+                        }
                     </button>
-                    <button className="btn btn-info btn-sm btn-block">
-                        ¿Ya tienes cuenta?
+                    <button className="btn btn-info btn-sm btn-block"
+                            onClick={() => setEsRegistro(!esRegistro)}
+                            type="button"
+                    >
+                        {
+                            esRegistro ? '¿Ya estas registrado?' : '¿No tienes cuenta?'
+                        }
                     </button>
                    </form>
                </div>
