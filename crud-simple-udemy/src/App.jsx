0ea2cedb277b670fb2316bc7f0d22aa13aeb0d7c
Pablo2e <hebenstreitpablo@hotmail.com>
2020-09-16 15:13:33 +0200

    crud simple agregado a package.son

--- crud-simple-udemy/src/App.jsx
+++ crud-simple-udemy/src/App.jsx
@@ -1,120 +1,47 @@
-import React from 'react';
-import shortid from 'shortid'
+import React from 'react'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Login from './components/Login'
+import Admin from './components/Admin'
 
-function App() {
+import {auth } from './firebase'
 
-  const [tarea, setTarea] = React.useState('')
-  const [tareas, setTareas] = React.useState([])
-  const [modoEdicion, setModoEdicion] = React.useState(false)
-  const [id, setId] = React.useState('')
-  const [error, setError] = React.useState(null)
 
-  const agregarTarea = e => {
-    e.preventDefault()
-    if(!tarea.trim()){
-      console.log('Elemento vacio')
-      setError('Escriba algo por favor')
-      return
-    }
-    console.log(tarea)
-    setTareas([
-      ...tareas,
-      {id: shortid.generate(), nombreTarea: tarea}
-    ])
-    setTarea('')
-    setError(null)
-  }
+const App = () => {
 
-  const eliminarTarea = id => {
-    const arrayFiltrado = tareas.filter (item => item.id !== id)
-    setTareas(arrayFiltrado)
-  }
+    const [firebaseUser, setFirebaseUser] = React.useState(false)
 
-  const editar = item => {
-    console.log(item)
-    setModoEdicion(true)
-    setTarea(item.nombreTarea)
-    setId(item.id)
-  }
-
-  const editarTarea = e => {
-    e.preventDefault()
-    if(!tarea.trim()){
-      console.log('Elemento vacio')
-      setError('Escriba algo por favor')
-      return
-    }
-    const arrayEditado = tareas.map(
-      item => item.id === id ? {id:id, nombreTarea:tarea} : item
-      )
-    setTareas(arrayEditado)
-    setModoEdicion(false)
-    setTarea('')
-    setId('')
-    setError(null)
-  }
-
-  return (
-    <div className="container mt-5">
-      <h1 className="text-center">CRUD Simple</h1>
-      <hr/>
-      <div className="row">
-        <div className="col-8">
-          <h4 className="text-center">Lista de tareas</h4>
-          <ul className="list-group">
-            {
-              tareas.length === 0 ? (
-                <li className="list-group-item">No hay tareas</li>
-              ) : (
-                tareas.map(item=>(
-                  <li className="list-group-item" key={item.id}>
-                  <span className="lead">{item.nombreTarea}</span>
-                  <button 
-                    className="btn btn-danger btn-sm float-right mx-2"
-                    onClick={() => eliminarTarea(item.id)}
-                    >Eliminar
-                  </button>
-                  <button 
-                    className="btn btn-warning btn-sm float-right"
-                    onClick={() => editar(item)}
-                    >Editar
-                  </button>
-                </li>
-                ))
-              )
+    React.useEffect(() => {
+        auth.onAuthStateChanged(user => {
+            console.log(user)
+            if(user){
+                setFirebaseUser(user)
+            }else{
+                setFirebaseUser(null)
             }
-          </ul>
-        </div>
-        <div className="col-4">
-          <h4 className="text-center">
-            {
-              modoEdicion ? 'Editar Tarea' : 'Agregar tarea'
-            }
-          </h4>
-          <form onSubmit={ modoEdicion ? editarTarea : agregarTarea }>
+        })
+    }, [])
 
-            {
-              error ? <span className="text-danger">{error}</span> : null
-            }
-            <input 
-              type="text" 
-              className="form-control mb-2"
-              placeholder="Ingrese tarea"
-              onChange={e => setTarea(e.target.value)}
-              value={tarea}
-            />
-            {
-              modoEdicion ? (
-                <button className="btn btn-warning btn-block" type="submit">Editar</button>
-              ) : (
-                <button className="btn btn-dark btn-block" type="submit">Agregar</button>
-              )
-            }
-          </form>
-        </div>
-      </div>
-    </div>
-  );
+    return firebaseUser !== false ? (
+        <Router>
+            <div className="container">
+                <Navbar firebaseUser={firebaseUser}/>
+                <Switch>
+                    <Route path="/login">
+                        <Login />
+                    </Route>
+                    <Route path="/admin">
+                        <Admin />
+                    </Route>
+                    <Route path="/" exact>
+                        Ruta de inicio
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    ): (
+        <div>Cargando...</div>
+    )
 }
 
-export default App;
+export default App
