77578dd5d9f204ee3e1e43ce96a3846b16268ad8
Pablo2e <hebenstreitpablo@hotmail.com>
2020-09-23 16:36:47 +0200

    Comienzo de Redux

---
+++ udemy-redux-1/src/redux/pokesDucks.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+
+//Constantes
+const dataInicial = {
+    array: []
+}
+
+const OBTENER_POQUEMONES_EXITO = 'OBTENER_POQUEMONES_EXITO'
+
+//Reducer
+export default function pokeReducer(state = dataInicial, action){
+    switch(action.type){
+            case OBTENER_POQUEMONES_EXITO:
+                return {...state, array: action.payload}
+            default:
+                return state
+    }
+}
+
+//Acciones
+export const obtenerPokemonesAccion = () => async (dispatch, getState) => {
+    try {
+        const res = await axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=20')
+        dispatch({
+            type: OBTENER_POQUEMONES_EXITO,
+            payload: res.data.result
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
