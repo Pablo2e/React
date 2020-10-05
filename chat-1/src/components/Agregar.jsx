619c31b6bf81b0a5fe5d2b5aa80299d11df5f682
Pablo2e <hebenstreitpablo@hotmail.com>
2020-10-05 16:12:25 +0200

    chat + context api (bases de datos en tiempo real) completo

--- chat-1/src/components/Agregar.jsx
+++ chat-1/src/components/Agregar.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import {ChatContext} from '../context/ChatProvider'
 
 const Agregar = () => {
+
+    const {agregarMensajes, usuario} = React.useContext(ChatContext)
+    const [mensaje, setMensaje] = React.useState('')
+
+    const agregar = e => {
+        e.preventDefault()
+        if(!mensaje.trim()){
+            console.log('texto vacío')
+            return
+        }
+        agregarMensajes(usuario.uid, mensaje)
+        setMensaje('')
+    }
+
     return (
         <form 
             className="fixed-bottom input-group p-3 bg-dark"
+            onSubmit={agregar}
+            value={mensaje}
+            onChange={e => setMensaje(e.target.value)}
         >
             <input
                 type="text"
