259605131a8a5a0ae165a105e589c3405fa45324
Pablo2e <hebenstreitpablo@hotmail.com>
2020-10-07 18:52:05 +0200

    Material UI, Material Design & React

---
+++ material-1/src/components/Contenedor.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import {
+    Hidden,
+    makeStyles,
+
+} from '@material-ui/core'
+import Navbar from './Navbar'
+import Cajon from './Cajon'
+import Cajita from './Cajita'
+
+const estilos = makeStyles(theme => ({
+    root: {
+        display: 'flex'
+    },
+    toolbar: theme.mixins.toolbar,// para que el alto del navbar sea siempre el mismo
+    content: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.default,
+        padding: theme.spacing(3),
+    },
+}))
+
+const Contenedor = (props) => {
+
+    const classes = estilos()
+    const [abrir, setAbrir] = React.useState(false)
+
+    const accionAbrir = () => {
+        setAbrir(!abrir)
+    }
+
+    return (
+        <div className={classes.root}>
+            <Navbar accionAbrir={accionAbrir}/>
+            <Hidden xsDown>
+                <Cajon
+                variant='permanent'
+                open={true}
+                />
+            </Hidden>
+            <Hidden smUp>
+                <Cajon
+                variant='temporary'
+                open={abrir}
+                onClose={accionAbrir}
+                />
+            </Hidden>
+            <div className={classes.content}>
+                <div className={classes.toolbar}></div>
+                <Cajita/>
+            </div>
+        </div>
+    )
+}
+
+export default Contenedor
--- material-1/src/components/Navbar.jsx
+++ material-1/src/components/Navbar.jsx
@@ -1,38 +1,49 @@
 import React from 'react'
 import {AppBar, Toolbar, Typography, makeStyles, IconButton, Button} from '@material-ui/core'
-import theme from '../temaConfig'
 import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles(theme => ({
-    offset: theme.mixins.toolbar,
     menuButton: {
       marginRight: theme.spacing(2),
+      [theme.breakpoints.up('sm')]:{
+          display: 'none',
+      },
     },
     title:{
         flexGrow: 1
-    }
+    },
+    appBar: {
+        [theme.breakpoints.up('sm')]:{
+        width: `calc(100% - ${240}px)`,
+        marginLeft: 240,
+        },
+    },
 }))
 
-const Navbar = () => {
+const Navbar = (props) => {
 
     const clases = useStyles()
     return (
-        <div>
-            <AppBar position="fixed" color="primary">
-              <Toolbar>
-                  <IconButton color="inherit" aria-label="menu" className={clases.menuButton}>
-                    <MenuIcon />
-                  </IconButton>
-                <Typography variant="h6" className={clases.title}>
-                  Bucmarcs
-                </Typography>
-                <Button variant="text" color="inherit">
-                  login
-                </Button>
-              </Toolbar>
-            </AppBar>
-            <div className={clases.offset}></div>
-        </div>
+        
+    <AppBar className={clases.appBar}>
+        <Toolbar>
+            <IconButton 
+                color="inherit" 
+                aria-label="menu" 
+                className={clases.menuButton}
+                onClick={() => props.accionAbrir()}
+            >
+            <MenuIcon />
+            </IconButton>
+        <Typography variant="h6" className={clases.title}>
+            Bucmarcs
+        </Typography>
+        <Button variant="text" color="inherit">
+            login
+        </Button>
+        </Toolbar>
+    </AppBar>
+        
     )
 }
 
